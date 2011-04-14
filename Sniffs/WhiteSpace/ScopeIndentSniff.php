a03a090fd8a538a31c3fae00346919ea39be2383
Eric J. Duran <eric.duran7@gmail.com>
2011-04-13 22:54:38 -0400

    Adding basic indetation sniffs

---
+++ Sniffs/WhiteSpace/ScopeIndentSniff.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * drupalcs_Sniffs_WhiteSpace_ScopeIndentSniff.
+ *
+ * @author    Eric J. Duran
+ */
+class drupalcs_Sniffs_WhiteSpace_ScopeIndentSniff extends Generic_Sniffs_WhiteSpace_ScopeIndentSniff {
+
+    /**
+     * The number of spaces code should be indented.
+     *
+     * @var int
+     */
+    public $indent = 2;
+
+}
+
+?>
