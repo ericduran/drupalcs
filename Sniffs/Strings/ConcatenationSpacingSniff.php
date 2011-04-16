f8f26ce7df4e991aec87293c0a46fec05838a70b
Eric J. Duran <eric.duran7@gmail.com>
2011-04-16 19:47:44 -0400

    Adding String Concatenations sniff

---
+++ Sniffs/Strings/ConcatenationSpacingSniff.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * drupalcs_Sniffs_Strings_ConcatenationSpacingSniff.
+ *
+ * Always use a space between the dot and the concatenated parts to improve readability.
+ *
+ * @url http://drupal.org/coding-standards#concat
+ */
+class drupalcs_Sniffs_Strings_ConcatenationSpacingSniff implements PHP_CodeSniffer_Sniff {
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+  public function register() {
+    return array(T_STRING_CONCAT);
+  }
+
+  /**
+   * Processes this test, when one of its tokens is encountered.
+   */
+  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {        
+    $tokens = $phpcsFile->getTokens();
+    if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE || $tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
+      $message = 'Concat operator must be surrounded by spaces';
+      $phpcsFile->addError($message, $stackPtr, 'Missing');
+    }
+  }
+}
+
+?>
