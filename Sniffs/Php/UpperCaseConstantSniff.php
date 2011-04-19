a82fa8b4cc9136b0591e819d38b289afe15ad3a8
Eric J. Duran <eric.duran7@gmail.com>
2011-04-19 00:39:46 -0400

    Cosmetic fixes

--- Sniffs/Php/UpperCaseConstantSniff.php
+++ Sniffs/Php/UpperCaseConstantSniff.php
@@ -6,24 +6,19 @@
  */
 class drupalcs_Sniffs_PHP_UpperCaseConstantSniff extends Generic_Sniffs_PHP_LowerCaseConstantSniff {
 
-    /**
-     * Processes this sniff, when one of its tokens is encountered.
-     */
-    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
-    {
-        $tokens = $phpcsFile->getTokens();
+  /**
+   * Processes this sniff, when one of its tokens is encountered.
+   */
+  public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr) {
+    $tokens = $phpcsFile->getTokens();
 
-        $keyword = $tokens[$stackPtr]['content'];
-        if (strtoupper($keyword) !== $keyword) {
-            $error = 'TRUE, FALSE and NULL must be uppercase; expected "%s" but found "%s"';
-            $data  = array(
-                      strtoupper($keyword),
-                      $keyword,
-                     );
-            $phpcsFile->addError($error, $stackPtr, 'Found', $data);
-        }
-
+    $keyword = $tokens[$stackPtr]['content'];
+    if (strtoupper($keyword) !== $keyword) {
+      $error = 'TRUE, FALSE and NULL must be uppercase; expected "%s" but found "%s"';
+      $data  = array(strtoupper($keyword), $keyword);
+      $phpcsFile->addError($error, $stackPtr, 'Found', $data);
     }
+  }
 }
 
 ?>
